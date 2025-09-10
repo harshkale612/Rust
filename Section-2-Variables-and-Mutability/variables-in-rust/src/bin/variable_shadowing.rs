98f5f4f72df29e9cc0892f3a8a91ca7443ab965e
Harshavardhan <harshkale612@gmail.com>
2025-09-10 23:57:28 +0530

    add varible_shadowing file

---
+++ Section-2-Variables-and-Mutability/variables-in-rust/src/bin/variable_shadowing.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let grams_of_protin = "12.21";
+    println!("{grams_of_protin}");
+
+    let grams_of_protin = 12.21;
+    println!("{grams_of_protin}");
+
+    let mut grams_of_protin = 12;
+    println!("{grams_of_protin}");
+
+    grams_of_protin = 18;
+    println!("{grams_of_protin}");
+}
