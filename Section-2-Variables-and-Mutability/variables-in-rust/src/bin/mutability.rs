578dec58fdec3016dcd38df0dfa8f4778744b6fb
Harshavardhan <harshkale612@gmail.com>
2025-09-10 23:45:51 +0530

    Update bin

---
+++ Section-2-Variables-and-Mutability/variables-in-rust/src/bin/mutability.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut study_hours = 6;
+    println!("I have planned to study for {study_hours}");
+    study_hours = 8;
+    println!("Now, i have panned to study for {study_hours}");
+}
