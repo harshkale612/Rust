1e1f282b714ec4317f252ce68009ac0d683f5117
Harshavardhan <harshkale612@gmail.com>
2025-09-10 23:03:43 +0530

    Update main.rs

--- Section-2-Variables-and-Mutability/variables-in-rust/src/main.rs
+++ Section-2-Variables-and-Mutability/variables-in-rust/src/main.rs
@@ -1,6 +1,14 @@
-let apples = 21;
-let banana = 12;
-let friuts = apples + banana
-let is_admin = true;
-let user = "Harsh";
+fn main() {
+    let apples = 21;
+    let oranges = 12;
+    let is_admin = true;
+    let user = "Harsh";
 
+    println!("I have {} apples", apples);
+    println!("I have {oranges} oranges");
+    println!("I have {apples} apples and {oranges} oranges");
+    println!("I have {} apples and {} oranges", apples, oranges);
+    println!("I have {0} apples and {1} oranges", apples, oranges);
+    println!("Is admin {is_admin}");
+    println!("{user} is User");
+}
