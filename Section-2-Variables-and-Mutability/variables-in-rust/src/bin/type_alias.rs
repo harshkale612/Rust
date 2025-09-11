437b8c26d16eeb7c90a0dd948d7deee35f47c0ca
Harshavardhan <harshkale612@gmail.com>
2025-09-11 20:02:23 +0530

    add type

---
+++ Section-2-Variables-and-Mutability/variables-in-rust/src/bin/type_alias.rs
@@ -0,0 +1,7 @@
+type Meter = i32;
+
+fn main() {
+    let board_length: Meter = 18;
+    let board_breadth: Meter = 12;
+    println!("I have board whose length is {board_length}m and breadth is {board_breadth}m");
+}
