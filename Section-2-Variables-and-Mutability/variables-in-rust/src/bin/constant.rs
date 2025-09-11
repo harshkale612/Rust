3de736bf94a84321b6809d84021c67f8294931a9
Harshavardhan <harshkale612@gmail.com>
2025-09-11 19:44:18 +0530

    Update constant.rs

--- Section-2-Variables-and-Mutability/variables-in-rust/src/bin/constant.rs
+++ Section-2-Variables-and-Mutability/variables-in-rust/src/bin/constant.rs
@@ -1,5 +1,6 @@
 const TAX_RATE: f64 = 6.12;
 
 fn main() {
-    println!("{TAX_RATE} percent tax is going to apply your salary");
+    let my_income: i32 = 100;
+    println!("My income is {my_income} and {TAX_RATE} percent tax is going to apply my income");
 }
