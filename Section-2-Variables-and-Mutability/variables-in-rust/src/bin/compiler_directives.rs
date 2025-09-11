ce0b0d3820b5f8ddb9d5320fbcfb8552ba8e756c
Harshavardhan <harshkale612@gmail.com>
2025-09-11 20:15:19 +0530

    add compiler_directives.rs

---
+++ Section-2-Variables-and-Mutability/variables-in-rust/src/bin/compiler_directives.rs
@@ -0,0 +1,8 @@
+#![allow(unused_variables)]
+const GAME_TIME: i32 = 40;
+//#[allow(unused_variables)] -> Function level
+fn main() {
+    //  #[allow(unused_variables)] -> Variable level
+    let score = 12;
+    println!("Hello World!!!");
+}
